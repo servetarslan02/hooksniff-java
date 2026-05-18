dab34cd5e4d6d97155d320386fc1192be48387c1
servetarslan02 <servetarslan02@gmail.com>
2026-05-19 03:04:06 +0800

    feat: timeoutMs, headers added to HookSniffOptions

--- lib/src/main/java/com/hooksniff/HookSniffOptions.java
+++ lib/src/main/java/com/hooksniff/HookSniffOptions.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Setter
 @Getter
@@ -14,4 +16,31 @@ public final class HookSniffOptions {
     private String serverUrl;
     private final List<Long> retrySchedule = Arrays.asList(1000L, 2000L, 4000L);
     private boolean debug = false;
+    private long timeoutMs = 30000;
+    private Map<String, String> headers = new HashMap<>();
+
+    public HookSniffOptions serverUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+        return this;
+    }
+
+    public HookSniffOptions debug(boolean debug) {
+        this.debug = debug;
+        return this;
+    }
+
+    public HookSniffOptions timeoutMs(long timeoutMs) {
+        this.timeoutMs = timeoutMs;
+        return this;
+    }
+
+    public HookSniffOptions header(String name, String value) {
+        this.headers.put(name, value);
+        return this;
+    }
+
+    public HookSniffOptions headers(Map<String, String> headers) {
+        this.headers.putAll(headers);
+        return this;
+    }
 }
